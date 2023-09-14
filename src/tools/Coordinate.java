b7da65ac4a7cda6b5d53bad54257fdff2df93696
frajavimopu <frajavimopu@gmail.com>
2023-09-14 16:48:46 -0600

    Add source code, licence and .gitignore files

---
+++ src/tools/Coordinate.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+
+/**
+ *
+ * @author TICS03
+ */
+public class Coordinate {
+    public static final float XMIN = -20.0f;
+    public static final float XMAX = 20.0f;
+    public static final float YMIN = -20.0f;
+    public static final float YMAX = 20.0f;
+    
+    public static final float XSCREEN = 300;
+    public static final float YSCREEN = 300; 
+    
+    public static int toScreenX(float x) {
+        //return (int) ( x * XSCREEN / XMAX );
+        return (int) (  (x - XMIN) * XSCREEN / (XMAX - XMIN)  );
+    }
+    
+    public static int toScreenY(float y) {
+        //return (int) ( (YMAX - y ) * YSCREEN / YMAX );
+        return (int) ( (y - YMAX ) * YSCREEN / (YMIN-YMAX) );
+    }
+}
