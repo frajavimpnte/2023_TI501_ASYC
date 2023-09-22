2b22f610516254d939d40262a0bd5568c36d7249
frajavimopu <frajavimopu@gmail.com>
2023-09-22 10:44:45 -0500

    Add toString method to ComplexNumber

--- src/tools/ComplexNumberTest.java
+++ src/tools/ComplexNumberTest.java
@@ -12,27 +12,32 @@ public class ComplexNumberTest {
     public static void main(String[] args) {
         // TP01 P1
         ComplexNumber a0 = new ComplexNumber(-1, 2);
-        System.out.println("a0" + a0);
+        System.out.println("a0: " + a0);
         
         ComplexNumber a1 = new ComplexNumber(4, 0);
-        System.out.println("a1" + a1);
+        System.out.println("a1: " + a1);
        
         ComplexNumber a2 = new ComplexNumber(1, -1);
-        System.out.println("a2" + a2);
+        System.out.println("a2: " + a2);
         
         ComplexNumber a3 = new ComplexNumber(2, -1);
-        System.out.println("a3" + a3);
+        System.out.println("a3: " + a3);
         
         ComplexNumber a4 = new ComplexNumber(1, 1);
-        System.out.println("a4" + a4);
+        System.out.println("a4: " + a4);
         
         ComplexNumber a0_2 = a0.toN(2);
-        System.out.println("a0_2" + a0_2);
+        System.out.println("a0_2: " + a0_2);
 
         
         ComplexNumber div0 = a1.div(a2);
+        System.out.println("div0: " + div0);
+        
         ComplexNumber div1 = a3.div(a4);
+        System.out.println("div1: " + div1);
+        
         ComplexNumber suma = div0.add(div1);
+        System.out.println("suma: " + suma);
         ComplexNumber TP01_P1 = a0_2.mult(suma);
         
         System.out.println("TP01_P1: " + TP01_P1.getA() + 
